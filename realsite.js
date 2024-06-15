78ad078520976263cb2da67d06b577f65af1979c
UserNameProgrammist <dmitrychoo@mail.ru>
2024-06-15 18:15:19 +0300

    Add files via upload

---
+++ realsite.js
@@ -0,0 +1,73 @@
+
+
+//   let number = 5 ранее
+
+let number = 0
+
+//   while (number > 0) {
+//       console.log("Hello World")
+//       number = number - 1 
+// }
+
+
+// do {
+//     console.log("Hello World")
+//     number = number - 1 
+
+    
+// } while (number > 0) 
+
+// ду вайл отработает как минимум один раз даже если условие не выполняется наверное потому что вайл после ду
+
+
+
+
+// for (let i = 0; i < 3; i++) { /// ай плюс плюс это значит ай плюс один
+//     alert(i); // просто вывод в браузер прибавления чисел
+// } // выводится ноль и после этого прибавляются числа
+
+for (let i = 0; i < 3; i=i+2) { /// ай плюс плюс это значит ай плюс один
+    alert(i); // просто вывод в браузер прибавления чисел
+} // выводится ноль и после этого прибавляются числа
+
+
+
+// // начало	let i = 0	Выполняется один раз при входе в цикл
+// // условие	i < 3	Проверяется перед каждой итерацией цикла.
+// // Если оно вычислится в false, цикл остановится.
+// // тело	alert(i)	Выполняется снова и снова, пока условие вычисляется в true.
+// // шаг	i++	Выполняется после тела цикла на каждой итерации перед проверкой условия.
+
+// const input = document.querySelector("input")
+// const button = document.querySelector("button")
+
+// const todos = ['task1', 'task2', 'task3']
+// todos[3] = 'task4'
+// console.log(todos[3])
+// console.log(todos.length)
+// todos.push('task5')
+// console.log(todos)
+// for (const el of todos) {
+//   console.log(el)
+//   const h2 = document.createElement('h2');
+//   h2.innerHTML = el
+//   document.body.append(h2)
+// }
+// // конст эль оф тодос переменная внутри цикла каждый раз призапуске цикла выводит следующее значение массива
+
+// button.addEventListener('click', function () {
+//   const h2 = document.createElement('h2');
+//   h2.innerHTML = input.value
+//   document.body.append(h2)
+
+  
+// })
+
+
+// HW ПОВТОРИТЬ ВСЁ ПЛЮС БУДУТ ЗАДАЧИ В ВАТСАПП НА ЦИКЛЫ
+
+
+
+
+
+
